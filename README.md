9a326512522c8178443110eee14cf58dba25200d
Ben Mazzotta <benjamin.mazzotta@tufts.edu>
2014-10-15 13:09:47 -0400

    Hello World

---
+++ README.md
@@ -0,0 +1,7 @@
+## Gini Coefficients
+
+This project seeks to recover lognormal distributions (mu, sigma) from known national values of national income, Gini coefficients, and national income per capita.
+
+The World Bank reports time series cross-sectional data for GNI, GNI per capita, and Gini coefficients for a wide range of countries.
+
+This project fits lognormal income distributions that match the data for each country, creating maximum likelihood estimates of personal income based on the World Bank data.
---
+++ scripts/import WDI data.R
@@ -0,0 +1,53 @@
+## Ben Mazzotta
+## Gini Coefficients
+## Import WDI data
+## 10/15/2014
+
+# #####
+# 0.  Preliminaries
+require(WDI); require(ggplot2); require(Hmisc); require(plyr)
+
+
+# ##### 
+# 1.  Gather GNI and Gini data
+
+v.income <- WDIsearch(string="GNI")[26] ## Income, national, current US$
+v.inccap <- WDIsearch(string="GNI")[30] ## Income per capita
+v.savingrate <- WDIsearch(string="Gross savings")[4] ## Savings as a percent of income
+v.saving <- WDIsearch(string="Gross savings")[2] ## Savings, current US$
+v.gini <- WDIsearch("GINI index") ## Gini index, a pure number bounded on (0,1)
+
+vars.wdi <- c(v.income, v.inccap, v.savingrate, v.saving, v.gini)
+
+income <- WDI(indicator = vars.wdi,start = 2000, end=2012)
+##      Remember ... if you later want GROUPs such as lending, income, etc. you can add `, extra=TRUE` above)
+
+str(income)
+summary(income)
+
+##      Goal: get a single estimate of income, income per capita, and Gini coefficient by year.
+
+require(data.table)
+inc.dt <- data.table(income)
+
+##      Take the averages by country over all 13 years 2000-2012; this is just to test.
+inc.dt[, income:= mean(get(v.income), na.rm=T), by="country"]
+inc.dt[, inccap:= mean(get(v.inccap), na.rm=T), by="country"]
+inc.dt[, gini:= mean(get(v.gini), na.rm=T), by="country"]
+inc.dt[, saving:= mean(get(v.saving), na.rm=T), by="country"]
+inc.dt[, savrate:= mean(get(v.savingrate), na.rm=T), by="country"]
+
+##      Make a single year in cross-section, using the scrubbed averages.
+inc2010 <- inc.dt[year==2010,.SD, .SDcols=c("iso2c","country","year","income","inccap","gini","saving","savrate")]
+str(inc2010); summary(inc2010)
+
+inc2010[!is.na(gini), country]
+
+# #####
+# Save and clean up 
+
+save(inc.dt, file="./data/income_firstcut.Rda")
+save.image("./data/current.Rdata")
+save.image("./data/working.Rdata")
+save.image("./data/archive 20141015.Rdata")
+save.image(".Rdata")
