d3c328f538d353a98dded62773988b4a701eb8b8
Ben Mazzotta <benjamin.mazzotta@tufts.edu>
2014-10-16 09:38:16 -0400

    Added saving rates.

--- scripts/import WDI data.R
+++ scripts/import WDI data.R
@@ -16,11 +16,13 @@ v.inccap <- WDIsearch(string="GNI")[30] ## Income per capita
 v.savingrate <- WDIsearch(string="Gross savings")[4] ## Savings as a percent of income
 v.saving <- WDIsearch(string="Gross savings")[2] ## Savings, current US$
 v.gini <- WDIsearch("GINI index") ## Gini index, a pure number bounded on (0,1)
+v.savers <- WDIsearch("Saved at a financial institution in the past year")[1] ## Savers as a share of population
 
-vars.wdi <- c(v.income, v.inccap, v.savingrate, v.saving, v.gini)
+vars.wdi <- c(v.income, v.inccap, v.savingrate, v.saving, v.gini, v.savers)
 
-income <- WDI(indicator = vars.wdi,start = 2000, end=2012)
-##      Remember ... if you later want GROUPs such as lending, income, etc. you can add `, extra=TRUE` above)
+income <- WDI(indicator = vars.wdi,start = 2000, end=2012, extra=TRUE)
+income <- subset(income, region !="Aggregates")
+table(income$region)
 
 str(income)
 summary(income)
@@ -36,9 +38,10 @@ inc.dt[, inccap:= mean(get(v.inccap), na.rm=T), by="country"]
 inc.dt[, gini:= mean(get(v.gini), na.rm=T), by="country"]
 inc.dt[, saving:= mean(get(v.saving), na.rm=T), by="country"]
 inc.dt[, savrate:= mean(get(v.savingrate), na.rm=T), by="country"]
+inc.dt[, savers:= mean(get(v.savers), na.rm=T), by="country"]
 
 ##      Make a single year in cross-section, using the scrubbed averages.
-inc2010 <- inc.dt[year==2010,.SD, .SDcols=c("iso2c","country","year","income","inccap","gini","saving","savrate")]
+inc2010 <- inc.dt[year==2010,.SD, .SDcols=c("iso2c","country","year","income","inccap","gini","saving","savrate", "savers")]
 str(inc2010); summary(inc2010)
 
 inc2010[!is.na(gini), country]
